5013d432cfce634580f0d1f12a809b5c967ff643
eric2788 <tsukiko852@gmail.com>
2022-05-08 18:32:49 +0800

    added name column and update per transaction log

--- src/main/java/org/dragonitemc/dragoneconomy/DragonEconomyListener.java
+++ src/main/java/org/dragonitemc/dragoneconomy/DragonEconomyListener.java
@@ -1,9 +1,11 @@
 package org.dragonitemc.dragoneconomy;
 
 import com.ericlam.mc.eld.services.ScheduleService;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.dragonitemc.dragoneconomy.api.TransactionLogEvent;
 import org.dragonitemc.dragoneconomy.db.EconomyUser;
 import org.dragonitemc.dragoneconomy.repository.EconomyUserRepository;
 
@@ -21,23 +23,28 @@ public class DragonEconomyListener implements Listener {
     private DragonEconomy dragonEconomy;
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
+    public void onTransactionLog(TransactionLogEvent e) {
+
+        var uuid = e.getLog().getTarget().getId();
+        var name = Bukkit.getOfflinePlayer(uuid).getName();
+
         scheduleService.runAsync(dragonEconomy, () -> {
 
-            var user = repository.findById(e.getPlayer().getUniqueId()).orElseGet(() -> {
+            var user = repository.findById(uuid).orElseGet(() -> {
                 var u = new EconomyUser();
-                u.setId(e.getPlayer().getUniqueId());
-                u.setName(e.getPlayer().getName());
+                u.setId(uuid);
                 u.setBalance(0.0);
                 return u;
             });
 
-            // update the player name
-            user.setName(e.getPlayer().getName());
+            if (user.getName().equals(name)){
+                return;
+            }
 
+            user.setName(name);
             repository.save(user);
 
-        }).thenRunSync(v -> dragonEconomy.getLogger().info("username updated for " + e.getPlayer().getName()));
+        }).thenRunSync(v -> dragonEconomy.getLogger().info("username updated for " + name));
     }
 
 }
